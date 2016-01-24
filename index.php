36547e8a2f61e266370cc90836836f7bf0602fce
Joe Reynolds <joereynolds952@gmail.com>
2016-01-24 13:38:36 -0500

    better html and css

--- index.php
+++ index.php
@@ -1,12 +1,22 @@
 
-<div class="wrapper">
-    <p>Simple examples using modern CSS and HTML in place of JS (where possible).</p>
-    <a href="barchart.html">barchart</a>
-    <a href="modals.htm">modal popups</a>
-    <a href="slideshow.htm">slideshow</a>
-    <a href="tabs.html">tabs</a>
-    <p>All source code is contained within each example. To reproduce an example all you have to do is copy and paste the page source and it should just work.</p>
-</div>
+<section class="wrapper">
+  <h1>Utilities and Examples</h1>
+  <article class="example">
+      <h1>PHP</h1>
+      <p>Basic utilities I've made to make some things easier/quicker to do</p>
+      <a href=#>Blank</a>
+  </article>
+
+  <article class="example">
+      <h1>HTML CSS JS Examples</h1>
+      <p>Simple examples using modern CSS and HTML in place of JS (where possible).</p>
+      <a href="barchart.html">barchart</a>
+      <a href="modals.htm">modal popups</a>
+      <a href="slideshow.htm">slideshow</a>
+      <a href="tabs.html">tabs</a>
+      <p>All source code is contained within each example. To reproduce an example all you have to do is copy and paste the page source and it should just work.</p>
+  </article>
+</section>
 
 <style>
 
@@ -16,6 +26,16 @@ html {
 body {
   padding : 0;
   margin : 0
+}
+h1 {
+  color : red;
+  text-decoration : underline;
+}
+section h1 {
+  text-align : center;
+}
+article h1 {
+  text-align : initial;
 }
 .wrapper {
   font-family : arial; 
