f5849869ce443335cbe925d66790419bf7e78e57
Joe Reynolds <joereynolds952@gmail.com>
2016-01-24 08:23:52 -0500

    add index.php

---
+++ index.php
@@ -0,0 +1,38 @@
+
+<div class="wrapper">
+    <p>Simple examples using modern CSS and HTML in place of JS (where possible).</p>
+    <a href="barchart.html">barchart</a>
+    <a href="modals.htm">modal popups</a>
+    <a href="slideshow.htm">slideshow</a>
+    <a href="tabs.html">tabs</a>
+    <p>All source code is contained within each example. To reproduce an example all you have to do is copy and paste the page source and it should just work.</p>
+</div>
+
+<style>
+
+html {
+  background : #f7f7f7;
+}
+body {
+  padding : 0;
+  margin : 0
+}
+.wrapper {
+  font-family : arial; 
+  width : 600px;
+  margin : auto;
+  margin-top : 25px;
+  padding : 25px;
+  background : white;
+  border : 1px solid #ccc;   
+}
+a {
+  color : #aaa;
+  display : block;
+  text-decoration : underline;
+  transition : color .5s;
+}
+a:hover{
+  color : red;
+}
+</style>
