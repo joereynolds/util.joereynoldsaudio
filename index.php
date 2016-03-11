a82907aa4f295e0d881e0545e45ade00084bbed2
Joe Reynolds <joereynolds952@googlemail.com>
2016-03-11 15:21:01 +0000

    cleanup and add validation

--- index.php
+++ index.php
@@ -1,58 +1,30 @@
-<section class="wrapper">
-  <h1>Utilities and Examples</h1>
-  <article class="example">
-      <h1>PHP</h1>
-      <p>Basic utilities I've made to make some things easier/quicker to do</p>
-      <a href="photodata/">Photo data</a>
-      <a href="webgrep/">Webgrep</a>
-  </article>
+<html>
+<head>
+<title>JRA Utilities</title>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<main>
+  <section class="wrapper">
+    <h1>Utilities and Examples</h1>
+    <article class="example">
+        <h1>PHP</h1>
+        <p>Basic utilities I've made to make some things easier/quicker to do</p>
+        <a href="photodata/">Photo data</a>
+        <a href="webgrep/">Webgrep</a>
+    </article>
 
-  <article class="example">
-      <h1>HTML CSS JS Examples</h1>
-      <p>Simple examples using modern CSS and HTML in place of JS (where possible).</p>
-      <a href="barchart.html">barchart</a>
-      <a href="modals.htm">modal popups</a>
-      <a href="slideshow.htm">slideshow</a>
-      <a href="tabs.html">tabs</a>
-      <p>All source code is contained within each example. To reproduce an example all you have to do is copy and paste the page source and it should just work.</p>
-  </article>
-</section>
-
-<style>
-
-html {
-  background : #f7f7f7;
-}
-body {
-  padding : 0;
-  margin : 0
-}
-h1 {
-  color : red;
-  text-decoration : underline;
-}
-section h1 {
-  text-align : center;
-}
-article h1 {
-  text-align : initial;
-}
-.wrapper {
-  font-family : arial; 
-  width : 600px;
-  margin : auto;
-  margin-top : 25px;
-  padding : 25px;
-  background : white;
-  border : 1px solid #ccc;   
-}
-a {
-  color : #aaa;
-  display : block;
-  text-decoration : underline;
-  transition : color .5s;
-}
-a:hover{
-  color : red;
-}
-</style>
+    <article class="example">
+        <h1>HTML CSS JS Examples</h1>
+        <p>Simple examples using modern CSS and HTML in place of JS (where possible).</p>
+        <a href="snippets/barchart.htm">barchart</a>
+        <a href="snippets/modals.htm">modal popups</a>
+        <a href="snippets/slideshow.htm">slideshow</a>
+        <a href="snippets/tabs.htm">tabs</a>
+        <a href="snippets/validation.htm">validation</a>
+        <p>All source code is contained within each example. To reproduce an example all you have to do is copy and paste the page source and it should just work.</p>
+    </article>
+  </section>
+</main>
+</body>
+</html>
