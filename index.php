2f7fc96f7ae89b07b485b07add6efa0e48f9856c
Joe Reynolds <joereynolds952@gmail.com>
2016-03-16 21:04:31 +0000

    better css, work on imagemaker

--- index.php
+++ index.php
@@ -15,6 +15,24 @@ $container['view'] = function($container) {
     return $view;
 };
 
+$app->get('/', function($request, $response, $args) {
+    return $this->view->render($response, 'index.phtml');
+})->setName('index');
+
+$app->get('/util', function($request, $response, $args) {
+    return $response->getBody()->write("hello");
+});
+
+$app->get('/util/imagemaker', function($request, $response, $args) {
+    return $this->view->render($response, 'imagemaker.phtml',
+        ['title' => 'Imagemaker']
+    );
+});
+
+$app->post('/util/imagemaker', function($request, $response, $args) {
+    return $response->getBody()->write(var_dump($_POST));
+});
+
 //slightly hacky route for html experiments
 $app->get('/snippets/{snippet}', function($request, $response, $args) {
     return $this->view->render($response, $args['snippet'] . '.phtml',
@@ -24,8 +42,5 @@ $app->get('/snippets/{snippet}', function($request, $response, $args) {
     );
 });
 
-$app->get('/', function($request, $response, $args) {
-    return $this->view->render($response, 'index.phtml');
-})->setName('index');
 
 $app->run();
