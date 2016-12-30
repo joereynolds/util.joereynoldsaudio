d1fcf28e7820892fbe95e2c213e449ea1ce9c4ea
Joe Reynolds <joe.reynolds@sykescottages.co.uk>
2016-12-30 09:32:44 +0000

    remove half-finished ideas

--- ajax/photodataDelete.php
+++
@@ -1,4 +0,0 @@
-<?php
-//Fudge a DELETE request
-parse_str(file_get_contents('php://input'), $data);
-unlink('../' . $data['name']);
--- interface/factory.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace jra\factories;
-
-interface iFactory
-{
-    public function make();
-}
